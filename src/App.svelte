b4a015c95f43e5e0b8b698269c8b9499a278f8e1
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-16 03:28:02 +0700

    feat: add ready schedule

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-  import logo from './assets/koyori-excited.png';
+  import koyori from './assets/koyori-excited.png';
 </script>
 
 <div id="root">
-  <img src={logo} alt="Koyori Excited" />
+  <img src={koyori} alt="こより" />
   <h1>🧪 This website is under construction~!</h1>
   <p>
     This website is closed and currently in development until further
     <br />
     information. Please comeback again soon, ありがとう〜！
   </p>
+  <div id="info">
+    <h4>This website will be back soon and ready at :</h4>
+    <h2>December 31, 2021 16:00:00 JST</h2>
+  </div>
 </div>
 <div id="footer">
   <h6 id="art-credits">
@@ -38,6 +42,15 @@
     margin-top: 0.5rem;
     font-size: 1.1rem;
   }
+  #info h4 {
+    margin-top: 0.7rem;
+    font-size: 1.3rem;
+  }
+  #info h2 {
+    margin-top: 0.4rem;
+    font-size: 1.5rem;
+    color: rgb(124, 119, 119);
+  }
   #footer h6 {
     margin-top: 0.13rem;
     color: rgb(123, 120, 120);
@@ -51,6 +64,8 @@
   }
   #footer a:hover {
     color: rgb(45, 45, 153);
+    text-decoration: underline;
+    text-decoration-style: dotted;
   }
   img {
     margin-top: 2rem;
