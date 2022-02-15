c5ac77a97f751d825215496b49b10d7488ba4ff3
gifaldyazkaa <falcxxdev@pm.me>
2022-02-15 11:01:12 +0700

    style: code format

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,7 @@ const app = new App({
         name: 'Azur Lane',
         imgUrl:
           'https://img.shields.io/badge/Azur%20Lane-72493403%20(EN%20Avrora)-%233399ff?logo=codeship&style=for-the-badge',
-      }
+      },
     ],
     projects: [
       {
@@ -86,8 +86,7 @@ const app = new App({
       },
       {
         name: 'Dotfiles',
-        description:
-          'My dotfiles configuration for i3-gaps',
+        description: 'My dotfiles configuration for i3-gaps',
         repo_name: 'dotfiles',
         demo_url: 'None',
         img_preview: 'https://i.upload.systems/1pukVaLj',
