ebf5dc59d89ca84b103928ad339a7a739971329f
gifaldyazkaa <falcxxdev@pm.me>
2022-02-25 14:20:41 +0700

    chore: repo cleanup

--- src/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-function scrollHeader() {
-  const header = document.getElementById('header');
-  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 80) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader);
