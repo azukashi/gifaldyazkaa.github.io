5534d06663ebd5898c242ec67b75242fdb27ba76
Gifaldy Azka <falcxxdev@pm.me>
2022-03-08 04:23:51 +0700

    feat: configure nprogress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import NProgress from 'nprogress';
 	import Header from '$lib/Header.svelte';
+	import { navigating } from '$app/stores';
 	import '../assets/scss/global.scss';
 	import 'boxicons/css/boxicons.min.css';
+
+	NProgress.configure({
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <Header />
