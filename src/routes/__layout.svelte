b3a1585df6d9044748104f5f7ca6b6b993e1430a
Gifaldy Azka <falcxxdev@pm.me>
2022-03-17 01:58:14 +0700

    feat: import material-icons css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
 	import Header from '$lib/Header.svelte';
 	import { navigating } from '$app/stores';
 	import '../assets/scss/global.scss';
-	import 'boxicons/css/boxicons.min.css';
+	import 'material-icons/iconfont/outlined.css';
 
 	NProgress.configure({
 		minimum: 0.16
