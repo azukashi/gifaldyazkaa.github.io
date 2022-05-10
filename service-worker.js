39d59db555c0e21a8935555d76a940de57a395bf
gifaldyazkaa <gifaldyazkaa@users.noreply.github.com>
2022-05-10 05:39:12 +0000

    Deploying to gh-pages from  @ b295b91eae76bbd13697b5fb47d6253598917c9b 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const timestamp = 1652159770572;
+const timestamp = 1652161150582;
 const build = [
-  "/_app/start-21fe0b49.js",
+  "/_app/start-230fd389.js",
   "/_app/pages/__layout.svelte-b89f32a0.js",
   "/_app/assets/pages/__layout.svelte-83e27890.css",
   "/_app/pages/__error.svelte-47ef0619.js",
